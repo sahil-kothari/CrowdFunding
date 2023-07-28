86f21b73df3ebb1bfb22d4439ece688671c47e75
sahil-kothari <sahilckothari94@gmail.com>
2023-07-28 16:37:46 +0530

    replaced some icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import {Link,useNavigate} from 'react-router-dom';
 import {CustomButton} from './';
-import {logo,menu,search,thirdweb} from '../assets';
+import {logo,menu,search,thirdweb,profile_icon} from '../assets';
 import {navlinks} from '../constants';
 import { useStateContext } from '../context';
 
@@ -37,7 +37,7 @@ const Navbar = () => {
 
         <Link to="/profile">
           <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer"> 
-            <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain" />
+            <img src={profile_icon} alt="user" className="w-[68%] h-[68%] object-contain" />
           </div>
         </Link>
 
